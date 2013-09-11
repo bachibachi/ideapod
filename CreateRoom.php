3124c0ca4b874297ee0c466b0cd52ab340a871b3
bachibachi <yuyachiba@Yuya-no-MacBook-Pro.local>
2013-09-12 00:52:26 +0900

    部屋作成のhtml、phpファイル作成を行いました

---
+++ CreateRoom.php
@@ -0,0 +1,15 @@
+<?php
+
+	$roomName = $_POST['roomname'];
+	$share    = $_POST['share'];
+
+	try {
+		$createRoomPdo = new PDO("mysql:host=localhost;  dbname=ideapod; charset=utf8" "bachibachi", "bachibachi");
+		$query = "insert into room (roomname, share) values ('{$rooName}', '{$share}')";
+		$createRoomPdo->exec($query);
+	} catch(PDOException $e){
+		echo "失敗しました。<br>";
+	}
+
+
+?>
